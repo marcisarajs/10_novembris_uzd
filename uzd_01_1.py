7652600ba921b2892af47d25f28960156536ac58
marcisarajs <marcis.arajs@grobinasskola.lv>
2024-11-27 14:14:26 +0200

    Izpildīju pirmo uzdevumu.

--- uzd_01_1.py
+++ uzd_01_1.py
@@ -1,3 +1,9 @@
 # Uzdevums: 01-1: Izveidot programmu, kas prasa lietotâjam ievadît skaitli n, tad programma aprçíina n+nn+nnn. 
-Rezultâts tiek parâdîts konsolç.
+#Rezultâts tiek parâdîts konsolç.
+n = int(input("Ievadi skaitli n:"))
+nn = int(n * 2)
+nnn = int(n * 3)
+Atbilde = n+nn+nnn
+print("Tu ievadīji skaitli", n)
+print("n+nn+nnn = ", Atbilde)
 
