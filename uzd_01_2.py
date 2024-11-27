6281e185eb42a638723738dbe9b0b083c1572b01
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2024-11-27 08:20:57 +0200

    Update uzd_01_2.py

--- uzd_01_2.py
+++ uzd_01_2.py
@@ -3,12 +3,3 @@
 # laukums = 2 * (3.14 * râdiuss * râdiuss) + augstums * (2 * 3.14 * râdiuss)
 
 
-
-radiuss=float(input('Ievadi radiusu:'))
-augstums=float(input('Ievadi augstumu'))
-
-tilpums= 3.14*radiuss*radiuss*augstums
-laukums=2*(3.14*radiuss*radiuss)+augstums*(2*3.14*radiuss)
-
-print('Tilpums ir:',tilpums)
-print('Laukums ir:',laukums)
